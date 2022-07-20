1b10285fb5d0c643938b2b9e6409f0391423532f
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2022-07-20 13:27:14 +0700

    FEAT: Reconfigure code base

--- app/src/main/java/com/hightech/wifidetector/MainActivity.kt
+++ app/src/main/java/com/hightech/wifidetector/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var wifiManager: WifiManager
-    private var wifiIDs = ""
 
     companion object {
         const val REQUEST_CHANGE_WIFI_STATE = 1201
@@ -29,40 +28,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        wifiManager = baseContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        requestPermission {
+        requestWiFiPermission {
             scanWiFi()
         }
     }
 
-    private fun requestPermission(onSuccess: () -> Unit) {
-        val aflPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+    private fun requestWiFiPermission(callback: () -> Unit) {
         val aclPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        val aflPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
         val accessWiFiPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE)
         val changeWiFiPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CHANGE_WIFI_STATE)
 
-        val isPermissionGranted = aflPermission != PackageManager.PERMISSION_GRANTED ||
-                aclPermission != PackageManager.PERMISSION_GRANTED ||
+        val isPermissionsNotGranted = aclPermission != PackageManager.PERMISSION_GRANTED ||
+                aflPermission != PackageManager.PERMISSION_GRANTED ||
                 accessWiFiPermission != PackageManager.PERMISSION_GRANTED ||
                 changeWiFiPermission != PackageManager.PERMISSION_GRANTED
 
-        if (isPermissionGranted) {
+        if (isPermissionsNotGranted) {
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_WIFI_STATE,
                     Manifest.permission.CHANGE_WIFI_STATE
                 ),
                 REQUEST_CHANGE_WIFI_STATE
             )
         } else {
-            onSuccess.invoke()
+            callback.invoke()
         }
     }
 
     private fun scanWiFi() {
-        wifiIDs = ""
+        wifiManager = baseContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         val wifiScanReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
                 val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
@@ -88,6 +86,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun scanSuccess() {
         val results = wifiManager.scanResults
+        var wifiIDs = "WiFi Adapter Device: \n"
         for ((id, ap) in results.withIndex()) {
             val wifiID = "SSID=" + ap.SSID + " MAC=" + ap.BSSID
             Log.d("DEBUG FADHIL", wifiID)
@@ -101,6 +100,7 @@ class MainActivity : AppCompatActivity() {
         // consider using old scan results: these are the OLD results!
         val results = wifiManager.scanResults
         Log.d("DEBUG FADHIL", "OLD: $results")
+        binding.tvMain.text = String.format("WiFi scan failed")
     }
 
 }
