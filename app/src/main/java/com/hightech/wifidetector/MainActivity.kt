20a0721d2f9be9be83f4e1593b5a166d952245d7
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2022-07-20 15:00:39 +0700

    FEAT: Implement Builder Pattern to WiFiDetector

--- app/src/main/java/com/hightech/wifidetector/MainActivity.kt
+++ app/src/main/java/com/hightech/wifidetector/MainActivity.kt
@@ -1,106 +1,36 @@
 package com.hightech.wifidetector
 
-import android.Manifest
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.content.pm.PackageManager
-import android.net.wifi.WifiManager
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.hightech.wifidetector.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var wifiManager: WifiManager
-
-    companion object {
-        const val REQUEST_CHANGE_WIFI_STATE = 1201
-    }
+    private lateinit var wifiDetector: WiFiDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        requestWiFiPermission {
-            scanWiFi()
-        }
-    }
-
-    private fun requestWiFiPermission(callback: () -> Unit) {
-        val aclPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-        val aflPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-        val accessWiFiPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE)
-        val changeWiFiPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CHANGE_WIFI_STATE)
-
-        val isPermissionsNotGranted = aclPermission != PackageManager.PERMISSION_GRANTED ||
-                aflPermission != PackageManager.PERMISSION_GRANTED ||
-                accessWiFiPermission != PackageManager.PERMISSION_GRANTED ||
-                changeWiFiPermission != PackageManager.PERMISSION_GRANTED
-
-        if (isPermissionsNotGranted) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_WIFI_STATE,
-                    Manifest.permission.CHANGE_WIFI_STATE
-                ),
-                REQUEST_CHANGE_WIFI_STATE
-            )
-        } else {
-            callback.invoke()
-        }
-    }
-
-    private fun scanWiFi() {
-        wifiManager = baseContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        val wifiScanReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
-                if (success) {
-                    scanSuccess()
-                } else {
-                    scanFailure()
+        wifiDetector = WiFiDetector.Builder()
+            .activity(this)
+            .setOnScanResultListener(object : WiFiDetector.WiFiDetectorDelegate {
+                override fun onScanSuccess(data: String) {
+                    binding.tvMain.text = data
                 }
-            }
-        }
 
-        val intentFilter = IntentFilter()
-        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
-        baseContext.registerReceiver(wifiScanReceiver, intentFilter)
+                override fun onScanFailure(message: String, data: String?) {
+                    binding.tvMain.text = message
+                }
 
-        val success = wifiManager.startScan()
-        if (!success) {
-            // scan failure handling
-            scanFailure()
-        }
-
+            })
+            .build()
     }
 
-    private fun scanSuccess() {
-        val results = wifiManager.scanResults
-        var wifiIDs = "WiFi Adapter Device: \n"
-        for ((id, ap) in results.withIndex()) {
-            val wifiID = "SSID=" + ap.SSID + " MAC=" + ap.BSSID
-            Log.d("DEBUG FADHIL", wifiID)
-            wifiIDs += "$id. $wifiID \n"
-        }
-        binding.tvMain.text = wifiIDs
+    override fun onResume() {
+        super.onResume()
+        wifiDetector.detect()
     }
-
-    private fun scanFailure() {
-        // handle failure: new scan did NOT succeed
-        // consider using old scan results: these are the OLD results!
-        val results = wifiManager.scanResults
-        Log.d("DEBUG FADHIL", "OLD: $results")
-        binding.tvMain.text = String.format("WiFi scan failed")
-    }
-
 }
---
+++ app/src/main/java/com/hightech/wifidetector/WiFiDetector.kt
@@ -0,0 +1,140 @@
+package com.hightech.wifidetector
+
+import android.Manifest
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.wifi.WifiManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class WiFiDetector(
+    private val activity: Activity,
+    private val wifiManager: WifiManager,
+    private val onScanResultListener: WiFiDetectorDelegate
+) {
+
+    private constructor(builder: Builder) : this(
+        builder.activity!!,
+        builder.wifiManager!!,
+        builder.onScanResultListener!!
+    )
+
+    companion object {
+        const val REQUEST_CHANGE_WIFI_STATE = 1201
+    }
+
+    interface WiFiDetectorDelegate {
+        fun onScanSuccess(data: String)
+        fun onScanFailure(message: String, data: String? = null)
+    }
+
+    data class Builder(
+        var activity: Activity? = null,
+        var wifiManager: WifiManager? = null,
+        var onScanResultListener: WiFiDetectorDelegate? = null
+    ) {
+
+        fun activity(activity: Activity) = apply {
+            this.activity = activity
+            wifiManager = activity.baseContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        }
+
+        fun setOnScanResultListener(delegate: WiFiDetectorDelegate) = apply {
+            this.onScanResultListener = delegate
+        }
+
+        fun build() = WiFiDetector(this)
+
+    }
+
+    private fun requestWiFiPermission(callback: () -> Unit) {
+        val aclPermission = ContextCompat.checkSelfPermission(
+            activity.baseContext,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        val aflPermission = ContextCompat.checkSelfPermission(
+            activity.baseContext,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val accessWiFiPermission = ContextCompat.checkSelfPermission(
+            activity.baseContext,
+            Manifest.permission.ACCESS_WIFI_STATE
+        )
+        val changeWiFiPermission = ContextCompat.checkSelfPermission(
+            activity.baseContext,
+            Manifest.permission.CHANGE_WIFI_STATE
+        )
+
+        val isPermissionsNotGranted = aclPermission != PackageManager.PERMISSION_GRANTED ||
+                aflPermission != PackageManager.PERMISSION_GRANTED ||
+                accessWiFiPermission != PackageManager.PERMISSION_GRANTED ||
+                changeWiFiPermission != PackageManager.PERMISSION_GRANTED
+
+        if (isPermissionsNotGranted) {
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.CHANGE_WIFI_STATE
+                ),
+                REQUEST_CHANGE_WIFI_STATE
+            )
+        } else {
+            callback.invoke()
+        }
+    }
+
+    private fun scanWiFi() {
+        val wifiScanReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+                if (success) {
+                    scanSuccess(wifiManager)
+                } else {
+                    scanFailure(wifiManager)
+                }
+            }
+        }
+
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        activity.baseContext?.registerReceiver(wifiScanReceiver, intentFilter)
+
+        val success = wifiManager.startScan()
+        if (!success) {
+            // scan failure handling
+            scanFailure(wifiManager)
+        }
+
+    }
+
+    private fun scanSuccess(wifiManager: WifiManager) {
+        val results = wifiManager.scanResults
+        var wifiIDs = "WiFi Router Device: \n"
+        for ((id, ap) in results.withIndex()) {
+            val wifiID = "SSID=" + ap.SSID + " MAC=" + ap.BSSID
+            wifiIDs += "$id. $wifiID \n"
+        }
+        onScanResultListener.onScanSuccess(wifiIDs)
+    }
+
+    private fun scanFailure(wifiManager: WifiManager) {
+        // handle failure: new scan did NOT succeed
+        // consider using old scan results: these are the OLD results!
+        val results = wifiManager.scanResults
+        onScanResultListener.onScanFailure("WiFi scan failed", results.toString())
+    }
+
+    fun detect() {
+        requestWiFiPermission() {
+            scanWiFi()
+        }
+    }
+
+}
