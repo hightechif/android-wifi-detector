52001bacf1c3f163165fda0e2834eb592a44dffa
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2022-07-25 16:13:18 +0700

    FEAT: Setup notification

--- app/src/main/java/com/hightech/wifidetector/MainActivity.kt
+++ app/src/main/java/com/hightech/wifidetector/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.hightech.wifidetector
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.graphics.Color
+import android.media.RingtoneManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.hightech.wifidetector.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +16,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var wifiDetector: WiFiDetector
     private lateinit var registeredDevices: Map<String, List<String>>
+
+    companion object {
+        const val CHANNEL_ID = "MYCHANNEL"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +49,7 @@ class MainActivity : AppCompatActivity() {
                 override fun onDetectedSuccess() {
                     binding.tvDetectStatus.text = getString(R.string.detect_status_positive)
                     binding.tvDetectStatus.setTextColor(Color.GREEN)
+                    showNotification()
                 }
 
                 override fun onDetectedFailure() {
@@ -50,6 +61,31 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         wifiDetector.detect("Elevenia-BOD", listOf("c0:7b:bc:84:08:2d"))
+    }
+
+    fun showNotification() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+
+        val notificationChannel =
+            NotificationChannel(CHANNEL_ID, "name", NotificationManager.IMPORTANCE_LOW)
+
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 1, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val notificationBuilder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext)
+            .setContentText("Anda telah memasuki wilayah kantor EDTS")
+            .setContentTitle("Anda Login")
+            .setContentIntent(pendingIntent)
+            .addAction(android.R.drawable.sym_action_chat, "Title", pendingIntent)
+            .setChannelId(CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.sym_action_chat)
+
+        notificationBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+
+        val notification = notificationBuilder.build()
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+        notificationManager.notify(1, notification)
     }
 
 }
